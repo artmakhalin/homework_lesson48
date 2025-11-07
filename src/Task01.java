d36e98f3b082725f964e8083bc06f916ad78ead8
artmakhalin <artmakhalin@gmail.com>
2025-11-06 19:07:03 -0500

    added task1

--- src/Task01.java
+++ src/Task01.java
@@ -1,5 +1,30 @@
-public class Task01 {
-    public static void main(String[] args) {
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
+public class Task01 {
+    /*
+    Напишите программу, которая позволяет пользователю ввести список строк (например имена),
+    и по завершение (например, если пользователь ввел "exit" сохраняет этот список в файл strings.txt
+     */
+    public static void main(String[] args) {
+        writeToFile();
+    }
+
+    public static void writeToFile() {
+        try (Writer writer = new FileWriter("strings.txt");
+             Scanner scanner = new Scanner(System.in)) {
+            System.out.println("Enter text:");
+            String line;
+            while (!(line = scanner.nextLine()).equalsIgnoreCase("exit")) {
+                writer.write(line + System.lineSeparator());
+            }
+            System.out.println("Writing finished");
+        } catch (IOException e) {
+            System.out.println("Error when writing file: " + e.getMessage());
+        }
     }
 }
